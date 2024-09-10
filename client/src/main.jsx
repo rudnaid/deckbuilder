92c82043598b68feb39c553655f399723a8b39fc
Nagy Gergő <gergosculpture@gmail.com>
2024-09-10 13:59:36 +0200

    router v2 + errorPageComponent

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,25 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import DeckBuilder from "./components/DeckBuilder.jsx";
+import CardDisplay from "./components/CardDisplay.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/deckbuilder",
+    element: <DeckBuilder />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/carddisplay",
+    element: <CardDisplay />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
