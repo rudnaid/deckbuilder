a101388d96e5a2c0f6c105028150f190b27a4ab7
Ratkai Adam <adam.ratkai1@gmail.com>
2024-09-25 14:38:32 +0200

    deck css changes

--- client/src/components/Deck/Deck.jsx
+++ client/src/components/Deck/Deck.jsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
 import { useDrop } from "react-dnd";
-import CardCompact from '../CardCompact/CardCompact.jsx'
-import './Deck.css';
+import CardCompact from "../CardCompact/CardCompact.jsx";
+import "./Deck.css";
 import Trashcan from "../Trashcan.jsx";
 
 const saveDeck = (deck) => {
   const deckToSave = deck.map((card) => {
-    return { cardId: card._id, count: card.count }
+    return { cardId: card._id, count: card.count };
   });
-  return fetch('/api/deck/', {
-    method: 'POST',
+  return fetch("/api/deck/", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      Authorization: `${localStorage.getItem('token')}`
+      "Content-Type": "application/json",
+      Authorization: `${localStorage.getItem("token")}`,
     },
-    body: JSON.stringify(deckToSave)
-  }).then((res) => res.json())
-}
+    body: JSON.stringify(deckToSave),
+  }).then((res) => res.json());
+};
 
 function Deck({ onDrop }) {
   const [cardsInDeck, setCardsInDeck] = useState([]);
 
   const [{ canDrop, isOver }, drop] = useDrop(() => ({
-    accept: 'CARD',
+    accept: "CARD",
     drop: (item) => {
       onDrop(item);
-      
+
       setCardsInDeck((prevCards) => {
-        const cardIndex = prevCards.findIndex(card => card._id === item.card._id);
+        const cardIndex = prevCards.findIndex(
+          (card) => card._id === item.card._id
+        );
 
         if (cardIndex !== -1) {
           if (prevCards[cardIndex].count === 2) {
@@ -51,42 +53,57 @@ function Deck({ onDrop }) {
   }));
 
   const onSave = async (deck) => {
-    saveDeck(deck).then((res) => {
-      setCardsInDeck([])
-    })
-      .catch((error) => console.log(error))
-  }
+    saveDeck(deck)
+      .then((res) => {
+        setCardsInDeck([]);
+      })
+      .catch((error) => console.log(error));
+  };
 
   function handleTrashCanDrop(cardToDelete) {
-    setCardsInDeck((prevCards) => prevCards.filter(card => card._id !== cardToDelete._id));
+    setCardsInDeck((prevCards) =>
+      prevCards.filter((card) => card._id !== cardToDelete._id)
+    );
   }
 
   return (
     // <div className="deck-container" ref={drop}
     // >
     <>
-      <div className="deck"
-    
+      <div
+        className="deck"
         ref={drop}
         style={{
-          backgroundColor: isOver ? 'lightgreen' : 'burlywood',
-        }}>
+          backgroundColor: isOver ? "lightgreen" : "burlywood",
+        }}
+      >
+        <div>
+          <h3>Deck</h3>
+        </div>
 
         <div className="current-deck">
-        <h3>Deck</h3>
-          {cardsInDeck && cardsInDeck.map((card) => {
-            return <CardCompact key={card._id} card={card} count={card.count} />
-          })}
+          {cardsInDeck &&
+            cardsInDeck.map((card) => {
+              return (
+                <CardCompact key={card._id} card={card} count={card.count} />
+              );
+            })}
         </div>
-      <div className="deck-bottom">
-      <Trashcan onDelete={handleTrashCanDrop} />
-      <button onClick={() => { onSave(cardsInDeck) }}>Save deck</button>
-      </div>
+        <div className="deck-bottom">
+          <Trashcan onDelete={handleTrashCanDrop} />
+          <button
+            onClick={() => {
+              onSave(cardsInDeck);
+            }}
+          >
+            Save deck
+          </button>
+        </div>
       </div>
     </>
 
     // </div>
   );
-};
+}
 
 export default Deck;
