c74e85469a26ea878f5441a278863753376b14ef
rudnaid <david.rudnai@gmail.com>
2024-09-25 14:58:28 +0200

    add manacrystal img to filtersettings

--- client/src/components/FilterSettings/ManaCost.jsx
+++ client/src/components/FilterSettings/ManaCost.jsx
@@ -2,13 +2,17 @@ export default function ManaCost({ onClick }) {
   const totalCost = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <>
+    <div className="manaCrystalWrapper" >
       {totalCost.map((cost, idx) => (
-        <input type="button"
-          key={idx}
-          onClick={(e) => onClick('manaCost', e.target.value)}
-          value={cost}/>
+        <div 
+          key={idx} 
+          
+          onClick={() => onClick('manaCost', cost)}
+        >
+          <img src={'./mana-crystal.png'} alt={`Mana cost ${cost}`} className="manaCrystal"/>
+          <div className="manaCost">{cost}</div>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
