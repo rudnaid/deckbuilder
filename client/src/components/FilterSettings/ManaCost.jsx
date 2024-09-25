9ecf7a627d9cdcc2d02a6cbc1ce40f39ee5c8b80
rudnaid <david.rudnai@gmail.com>
2024-09-25 15:17:46 +0200

    fix manacrystal in cardcompact

--- client/src/components/FilterSettings/ManaCost.jsx
+++ client/src/components/FilterSettings/ManaCost.jsx
@@ -2,7 +2,7 @@ export default function ManaCost({ onClick }) {
   const totalCost = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
-    <div className="manaCrystalWrapper" >
+    <div className="manaCostWrapper" >
       {totalCost.map((cost, idx) => (
         <div 
           key={idx} 
