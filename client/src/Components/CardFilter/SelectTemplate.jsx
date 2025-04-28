37899585b8ab251e876fe677a6a97bd4031a127e
rudnaid <david.rudnai@gmail.com>
2025-04-28 17:38:30 +0200

    refactor(ManaCostSelector): change component from function declaration to arrow function, format code

--- client/src/Components/CardFilter/SelectTemplate.jsx
+++ client/src/Components/CardFilter/SelectTemplate.jsx
@@ -1,16 +1,24 @@
-export default function SelectTemplate({label, filter, objectKey, data, onChange, type}) {
-    return (
-      <label>
-        {label}
-        <select type="select" onChange={(e) => onChange(type, e.target.value)}>
-          <option value="all">All</option>
-          {data[filter].map((item, index) => (
-            <option key={index} value={item.id}>
-              {item[objectKey]}
-            </option>
-          ))}
-        </select>
-      </label>
-    );
-  }
-  
+const SelectTemplate = ({label, filter, objectKey, data, onChange, type}) => {
+  return (
+    <label>
+      {label}
+      <select
+        type="select"
+        onChange={(e) => onChange(type, e.target.value)}
+      >
+        <option value="all">All</option>
+
+        {data[filter].map((item, index) => (
+          <option
+            key={index}
+            value={item.id}
+          >
+            {item[objectKey]}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+}
+
+export default SelectTemplate;
